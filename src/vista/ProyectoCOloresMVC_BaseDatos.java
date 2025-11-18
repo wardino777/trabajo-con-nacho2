a93c375d77ae96ce1cbffd4a0762806c70576d6b
famil <famil@DESKTOP-SFU7N62>
2025-11-17 23:59:09 -0300

    puse hola

--- src/vista/ProyectoCOloresMVC_BaseDatos.java
+++ src/vista/ProyectoCOloresMVC_BaseDatos.java
@@ -9,6 +9,7 @@ public class ProyectoCOloresMVC_BaseDatos {
     public static void main(String[] args) 
     {
        //hola
+        //papu
        VentanaPrincipal ventPrin=new VentanaPrincipal();
        ventPrin.setLocationRelativeTo(null);
        ventPrin.setTitle("MENU PRINCIPAL");
